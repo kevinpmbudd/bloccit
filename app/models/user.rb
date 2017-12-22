04b040519bb88af54bf067587f099bc743aea1cf
Kevin Budd <kevin@kevinbudd.com>
2017-12-22 12:03:55 -0600

    checkpoint 29: completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
 
   before_save :normalize_email, :normalize_name, :assign_role
   before_save
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,8 @@ RSpec.describe User, type: :model do
 
   it { should have_many(:posts) }
   it { should have_many(:comments) }
-  
+  it { should have_many(:votes) }
+
   # shoulda tests for name
   it { should validate_presence_of(:name) }
   it { should validate_length_of(:name).is_at_least(1) }
